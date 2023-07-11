16ec06aa83c220e0ffcb1db0b392c2fa621ad612
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-07-11 17:18:39 -0400

    Login component
    Deleted css styling, added functionality to capture login, looking to integrate OAuth

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,10 @@ const App: React.FC = () => {
 
   return (
     <>
-      {/* <div className="card"> */}
-        {/* <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> */}
-        {/* <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-      {/* </div> */}
-      <Test imageSrc={reactIMG}/>
+      {/* <Test imageSrc={reactIMG}/>
       <Test imageSrc={'https://www.svgrepo.com/show/513696/banana.svg'}/>
-      <Test imageSrc={'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Vitejs-logo.svg/1039px-Vitejs-logo.svg.png'}/>
-      {/* <Login /> */}
-
+      <Test imageSrc={'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Vitejs-logo.svg/1039px-Vitejs-logo.svg.png'}/> */}
+      <Login />
     </>
   )
 }
